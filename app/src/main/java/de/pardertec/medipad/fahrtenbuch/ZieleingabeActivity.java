0fe446d0c18a21a02845340a63a3493e6d0e27a3
t.belmega@gmx.de <7B9deeyyUpjmqwNnX1Ej>
2016-03-02 20:36:00 +0000

    Rename zieleingabe_activity xml file

--- app/src/main/java/de/pardertec/medipad/fahrtenbuch/ZieleingabeActivity.java
+++ app/src/main/java/de/pardertec/medipad/fahrtenbuch/ZieleingabeActivity.java
@@ -17,7 +17,7 @@ public class ZieleingabeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_zieleingabe_activity);
+        setContentView(R.layout.activity_zieleingabe);
 
    }
 
